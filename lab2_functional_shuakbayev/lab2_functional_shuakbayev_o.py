c627db7c14773c9cca6a418fd1eabf3f24f1bf26
Carnivore <oshuakbaev>
2023-01-23 08:44:05 +0600

    lab2 shuakbayev

---
+++ lab2_functional_shuakbayev/lab2_functional_shuakbayev_o.py
@@ -0,0 +1,14 @@
+#lab2_functional_shuakbayev_o
+
+print("Shuakbayev Olzhas - IT age 20")
+
+if 5>2:
+	print("Aray Meirambaikyzy is the best teacher!");
+else:
+	print("also best as usually"); 
+
+username = input("Enter your name:")
+
+print("Username: "+username)
+
+
