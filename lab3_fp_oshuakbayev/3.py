5f89c352d30ad8a225c489b4801fc2a2e2c5e7ce
Carnivore <oshuakbaev>
2023-02-06 08:37:14 +0600

    updates

--- lab3_fp_oshuakbayev/3.py
+++ lab3_fp_oshuakbayev/3.py
@@ -1,12 +1,18 @@
-# используйте функции randint() randrange() random() enumerate()  в своей программе 
-import random 
+# # используйте функции randint() randrange() random() enumerate()  в своей программе 
+# import random 
 
-print(random.randint(0,5))
+# print(random.randint(0,5))
 
-print("Randrange: ")
+# print("Randrange: ")
 
-print(random.randrange(3, 9))
+# print(random.randrange(3, 9))
 
-print("random: ")
+# print("random: ")
 
-print(random.random())
+# print(random.random())
+
+
+b = "hello"
+
+for i in enumerate(b):
+	print(i)
